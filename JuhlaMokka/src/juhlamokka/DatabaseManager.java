488fe82610352290d5687d743b262f24ae4f55a6
Aleksi Hulkkonen <42401517+kaahulkkonen@users.noreply.github.com>
2018-08-15 13:53:19 +0300

    Initial DB connection support

--- JuhlaMokka/src/juhlamokka/DatabaseManager.java
+++ JuhlaMokka/src/juhlamokka/DatabaseManager.java
@@ -5,23 +5,56 @@
  */
 package juhlamokka;
 
+import java.sql.*;
+import java.util.Properties;
+
 /**
  *
  * @author s1800571
  */
 public class DatabaseManager {
+    public Connection connection = null;
+    
     private String host, username, password, database;
     private int port;
     
-    DatabaseManager(String host, int port, String username, String password, String database) {
+    DatabaseManager(String host, int port, String username, String password, String database) throws ClassNotFoundException {
        this.host = host;
        this.port = port;
        this.username = username;
        this.password = password;
        this.database = database;
+       
+       try
+       {
+           Class.forName("org.postgresql.Driver");
+       }
+       catch (ClassNotFoundException e)
+       {
+           throw new ClassNotFoundException("PostgreSQL JDBC driver NOT detected in library path.", e);
+       }
     }
     
-    public synchronized int Test() {
+    public void Connect() throws SQLException {
+        try {
+            String url = String.format("jdbc:postgresql://%s/%s", host, database);
+            
+            Properties properties = new Properties();
+            properties.setProperty("user", username);
+            properties.setProperty("password", password);
+            properties.setProperty("ssl", "true");
+            
+            connection = DriverManager.getConnection(url, properties);
+        } catch (SQLException e) {
+            throw new SQLException("Failed to create connection to database.", e);
+        }
+        
+        if (connection != null) {
+            System.out.println("Successfully created connection to database.");
+        }
+    }
+    
+    public int Test() {
         return 1;
     }
 }
--- JuhlaMokka/src/juhlamokka/JuhlaMokka.java
+++ JuhlaMokka/src/juhlamokka/JuhlaMokka.java
@@ -17,7 +17,7 @@ public class JuhlaMokka {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // TODO code application logic here
         System.out.println("Hello World");
         
@@ -28,5 +28,7 @@ public class JuhlaMokka {
                 "computing4523!",
                 "juhlamokka"
         );
+        
+        db.Connect();
     }
 }
